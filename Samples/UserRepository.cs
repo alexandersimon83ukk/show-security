079e4230f5539053a3c1fc8a17ccb45d07259d65
alexandersimon83ukk <alexander.simon@uk-koeln.de>
2026-03-18 21:06:06 +0100

    Implement GetUserDetails2 for user retrieval
    Added GetUserDetails2 method that retrieves user details based on username input.

--- Samples/UserRepository.cs
+++ Samples/UserRepository.cs
@@ -23,4 +23,24 @@ public class UserRepository
             }
         }
     }
+
+     public void GetUserDetails2(string userInputUsername)
+    {
+        string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            // CODEQL FINDING HERE:
+            // The variable 'userInputUsername' flows directly into the SQL query string.
+            string query = "SELECT * FROM Users WHERE Username = '" + userInputUsername + "'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                // ... process results ...
+            }
+        }
+    }
 }
